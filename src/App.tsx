3f0043d96a0ba4ffece412afe0ebcd4cee9ff13e
alvinlamAgridence <alvin.lam@agridence.com>
2025-02-01 13:10:13 +0800

    Set responsive bg image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
 import './App.css';
+import bgImage from './assets/bg-light.png';
 
 function App() {
   return (
-    <div className="App">
-      TEST
+    <div className='App'>
+      <div
+        className='Background'
+        style={{
+          backgroundImage: `url(${bgImage})`,
+        }}
+      >
+        Test
+      </div>
     </div>
   );
 }
